ec0807ba789058bc4e906066cafd26059ea67526
anushkaagarwal02 <113297001+anushkaagarwal02@users.noreply.github.com>
2023-09-17 19:10:48 +0530

    Time: 0 ms (100%), Space: 39.7 MB (58.78%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,15 +1,15 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        return cutie(nums,0,0);
+        return helper(nums,0,0);
     }
-    int cutie(int nums[],int idx,int xor)
+    int helper(int nums[],int idx,int xor)
     {
         if(idx==nums.length)
         {
             return xor;
         }
-        int in=cutie(nums,idx+1,xor^nums[idx]);
-        int nic=cutie(nums,idx+1,xor);
+        int in=helper(nums,idx+1,xor^nums[idx]);
+        int nic=helper(nums,idx+1,xor);
         return in+nic;
     }
 }
