501890c0d57df2719e791680e9bdd290d91a572f
anushkaagarwal02 <113297001+anushkaagarwal02@users.noreply.github.com>
2023-10-24 09:11:49 +0530

    Time: 1 ms (100%), Space: 45.2 MB (64.18%) - LeetHub

--- 1254-deepest-leaves-sum/1254-deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.java
@@ -14,28 +14,32 @@
  * }
  */
 class Solution {
-    public int deepestLeavesSum(TreeNode root) 
-    {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int sum=0;
-        while(!q.isEmpty())
-        {
-            int size=q.size();
-            int cursum=0;
-            for(int  i=0;i<size;i++)
-            {
-                TreeNode node=q.peek();
-                q.remove();
-                if(node.left != null) 
-                q.add(node.left);
-                if(node.right != null) 
-                q.add(node.right);
-                cursum += node.val;
-            }
-            sum = cursum;
-        }
+    public int deepestLeavesSum(TreeNode root) {
+        return util1(root);
+    }
+    int maxHeight = Integer.MIN_VALUE;
+    int sum = 0;
 
+    public int util1(TreeNode root) {
+        dfs(root, 0);
         return sum;
+    }
+
+    public void dfs(TreeNode root, int currHeight) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            if (currHeight > maxHeight) {
+                maxHeight = currHeight;
+                sum = root.val;
+            } 
+            else if (currHeight == maxHeight) 
+            {
+                sum += root.val;
+            }
+        }
+        dfs(root.left, currHeight + 1);
+        dfs(root.right, currHeight + 1);
     }
 }
